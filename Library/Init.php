e012a99b0eb16fbf8d7fbeddecf25f79281d4beb
ccbljty <changchunboeisr@163.com>
2017-04-17 11:23:01 +0800

    Update Init.php

--- Library/Init.php
+++ Library/Init.php
@@ -9,8 +9,18 @@ function __autoload($className) {
     $relPath = implode('/', explode('_', $className));
     require_once (APP_PATH . '/' . $relPath . '.php');
 }
-// 允许跨域访问
-header('Access-Control-Allow-Origin:*');
+
+// 允许携带信任证书
+header("Access-Control-Allow-Credentials: true");
+$crossHttps = Library_Conf::getConf('crossDomain');
+if(!empty($crossHttps) && is_array($crossHttps)) {
+    $servers = Library_Env::getServerParams();
+    if(in_array( $servers['SERVER_NAME'], $crossHttps)) {
+        // 允许跨域访问
+        header('Access-Control-Allow-Origin:http://' .  $servers['SERVER_NAME']);
+    }
+}
+
 // 开启会话
 session_start();
 // 设置时区
